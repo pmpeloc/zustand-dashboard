e7b294124ad0fe4716e098337573701eb0722134
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-11-05 01:04:11 -0300

    MODULE-1: Bases de Zustand

--- src/pages/01-basic/components/BearsDisplay.tsx
+++ src/pages/01-basic/components/BearsDisplay.tsx
@@ -6,11 +6,19 @@ import { useBearStore } from '../../../stores';
 export function BearsDisplay() {
   const bears = useBearStore(useShallow((state) => state.bears));
   const doNothing = useBearStore((state) => state.doNothing);
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
 
   return (
     <WhiteCard>
       <h1>Osos</h1>
-      <button onClick={doNothing}>Do Nothing</button>
+      <button onClick={doNothing}>No hacer nada</button>
+      <button className='mt-2' onClick={addBear}>
+        Agregar oso
+      </button>
+      <button className='mt-2' onClick={clearBears}>
+        Limpiar osos
+      </button>
       <pre>{JSON.stringify(bears, null, 2)}</pre>
     </WhiteCard>
   );
--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -10,17 +10,30 @@ interface BearState {
   polarBears: number;
   pandaBears: number;
   bears: Bear[];
+  computed: { totalBears: number };
   increaseBlackBears: (by: number) => void;
   increasePandaBears: (by: number) => void;
   increasePolarBears: (by: number) => void;
   doNothing: () => void;
+  addBear: () => void;
+  clearBears: () => void;
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
   blackBears: 10,
   pandaBears: 5,
   polarBears: 1,
   bears: [{ id: 1, name: 'Oso #1' }],
+  computed: {
+    get totalBears(): number {
+      return (
+        get().blackBears +
+        get().polarBears +
+        get().pandaBears +
+        get().bears.length
+      );
+    },
+  },
   increaseBlackBears: (by: number) =>
     set((state) => ({ blackBears: state.blackBears + by })),
   increasePandaBears: (by: number) =>
@@ -28,4 +41,12 @@ export const useBearStore = create<BearState>()((set) => ({
   increasePolarBears: (by: number) =>
     set((state) => ({ polarBears: state.polarBears + by })),
   doNothing: () => set((state) => ({ bears: [...state.bears] })),
+  addBear: () =>
+    set((state) => ({
+      bears: [
+        ...state.bears,
+        { id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` },
+      ],
+    })),
+  clearBears: () => set({ bears: [] }),
 }));
